a388464e3bc7e56c44076766578ded0aaa4169e6
Kevin Welch <kaedub16@gmail.com>
2025-12-03 23:44:44 -0800

    Set up eslint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', '*.js'],
+  },
+];
